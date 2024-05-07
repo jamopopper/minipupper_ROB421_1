7b0948acb70d5e41afcdf549e8b4e69b74275a80
jamopopper <jace_palmer@live.com>
2024-05-07 11:21:44 -0700

    more changes

--- jaceTest.py
+++ jaceTest.py
@@ -1,15 +1,15 @@
 import numpy as np
 import time
-from ..StanfordQuadruped.src.IMU import IMU
-from ..StanfordQuadruped.src.Controller import Controller
-from ..StanfordQuadruped.src.JoystickInterface import JoystickInterface
-from ..StanfordQuadruped.src.State import BehaviorState, State
-from ..StanfordQuadruped.MangDang.mini_pupper.HardwareInterface import HardwareInterface
-from ..StanfordQuadruped.MangDang.mini_pupper.Config import Configuration
-from ..StanfordQuadruped.pupper.Kinematics import four_legs_inverse_kinematics
-from ..StanfordQuadruped.MangDang.mini_pupper.display import Display
-from ..StanfordQuadruped.src.MovementScheme import MovementScheme
-from ..StanfordQuadruped.src.danceSample import MovementLib
+from StanfordQuadruped.src.IMU import IMU
+from StanfordQuadruped.src.Controller import Controller
+from StanfordQuadruped.src.JoystickInterface import JoystickInterface
+from StanfordQuadruped.src.State import BehaviorState, State
+from StanfordQuadruped.MangDang.mini_pupper.HardwareInterface import HardwareInterface
+from StanfordQuadruped.MangDang.mini_pupper.Config import Configuration
+from StanfordQuadruped.pupper.Kinematics import four_legs_inverse_kinematics
+from StanfordQuadruped.MangDang.mini_pupper.display import Display
+from StanfordQuadruped.src.MovementScheme import MovementScheme
+from StanfordQuadruped.src.danceSample import MovementLib
 
 def main(use_imu=False):
     """Main program
