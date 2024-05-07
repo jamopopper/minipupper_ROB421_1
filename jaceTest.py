4192904f985232405794ce824c37173b48765de4
jamopopper <jace_palmer@live.com>
2024-05-07 11:27:05 -0700

    more testin

--- jaceTest.py
+++ jaceTest.py
@@ -44,7 +44,65 @@ def main(use_imu=False):
     movementCtl = MovementScheme(MovementLib)
     dance_active_state = False
 
-    
+    last_loop = time.time()
+
+    print("Summary of gait parameters:")
+    print("overlap time: ", config.overlap_time)
+    print("swing time: ", config.swing_time)
+    print("z clearance: ", config.z_clearance)
+    print("x shift: ", config.x_shift)
+
+    # Wait until the activate button has been pressed
+    while True:
+        print("Waiting for L1 to activate robot.")
+        while True:
+            command = joystick_interface.get_command(state, disp)
+            joystick_interface.set_color(config.ps4_deactivated_color)
+            if command.activate_event == 1:
+                break
+            time.sleep(0.1)
+        print("Robot activated.")
+        joystick_interface.set_color(config.ps4_color)
+
+        while True:
+            now = time.time()
+            if now - last_loop < config.dt:
+                continue
+            last_loop = time.time()
+
+            # Parse the udp joystick commands and then update the robot controller's parameters
+            command = joystick_interface.get_command(state, disp)
+            if command.activate_event == 1:
+                print("Deactivating Robot")
+                disp.show_state(BehaviorState.DEACTIVATED)
+                break
+
+            # Read imu data. Orientation will be None if no data was available
+            quat_orientation = (
+                imu.read_orientation() if use_imu else np.array([1, 0, 0, 0])
+            )
+            state.quat_orientation = quat_orientation
+
+            # If "circle" button is clicked, switch dance_active_state between False/True.
+            if command.dance_activate_event == True:
+                if dance_active_state == False:
+                    dance_active_state = True
+                else:
+                    dance_active_state = False
+
+            # Step the controller forward by dt
+            if dance_active_state == True:
+            	# Caculate legsLocation, attitudes and speed using custom movement script
+                movementCtl.runMovementScheme()
+                legsLocation = movementCtl.getMovemenLegsLocation()
+                attitudes    = movementCtl.getMovemenAttitude()
+                speed        = movementCtl.getMovemenSpeed()
+                controller.run(state, command, disp, legsLocation, attitudes, speed)
+            else:
+                controller.run(state, command, disp)
+
+            # Update the pwm widths going to the servos
+            hardware_interface.set_actuator_postions(state.joint_angles)
 
 
 main()
