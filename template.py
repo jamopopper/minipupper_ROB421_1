8a4088947ecf6147dc0f6b801a7c5599f6c55124
jamopopper <jace_palmer@live.com>
2024-05-07 11:29:33 -0700

    template added

---
+++ template.py
@@ -0,0 +1,58 @@
+import sys
+
+sys.path.append('../StanfordQuadruped')
+
+import numpy as np
+import time
+from src.IMU import IMU
+from src.Controller import Controller
+from src.JoystickInterface import JoystickInterface
+from src.State import BehaviorState, State
+from MangDang.mini_pupper.HardwareInterface import HardwareInterface
+from MangDang.mini_pupper.Config import Configuration
+from pupper.Kinematics import four_legs_inverse_kinematics
+from MangDang.mini_pupper.display import Display
+from src.MovementScheme import MovementScheme
+from src.danceSample import MovementLib
+
+def main(use_imu=False):
+    """Main program
+    """
+
+    # Create config
+    config = Configuration()
+    hardware_interface = HardwareInterface()
+    disp = Display()
+    disp.show_ip()
+
+    # Create imu handle
+    if use_imu:
+        imu = IMU(port="/dev/ttyACM0")
+        imu.flush_buffer()
+
+    # Create controller and user input handles
+    controller = Controller(
+        config,
+        four_legs_inverse_kinematics,
+    )
+    state = State()
+    print("Creating joystick listener...")
+    joystick_interface = JoystickInterface(config)
+    print("Done.")
+
+    #Create movement group scheme instance and set a default false state
+    movementCtl = MovementScheme(MovementLib)
+    dance_active_state = False
+
+    last_loop = time.time()
+
+    print("Summary of gait parameters:")
+    print("overlap time: ", config.overlap_time)
+    print("swing time: ", config.swing_time)
+    print("z clearance: ", config.z_clearance)
+    print("x shift: ", config.x_shift)
+
+
+
+
+main()
