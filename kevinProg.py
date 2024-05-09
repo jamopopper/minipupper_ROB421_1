497086414db3d01eb6f7703255316fefeff79600
sabbekosu <sabbek@oregonstate.edu>
2024-05-09 10:10:09 -0700

    wee2

--- kevinProg.py
+++ kevinProg.py
@@ -1,11 +1,21 @@
-from Command import Command
-from Controller import Controller
-from State import BehaviorState, State
+import sys
+
+sys.path.append('../StanfordQuadruped')
+
 import numpy as np
 import time
+import os
+import sys
+from src.IMU import IMU
+from src.Controller import Controller
+from src.JoystickInterface import JoystickInterface
+from src.State import BehaviorState, State
 from MangDang.mini_pupper.HardwareInterface import HardwareInterface
 from MangDang.mini_pupper.Config import Configuration
-from pupper.Kinematics import four_legs_inverse_kinematics
+from pupper.Kinematics import four_legs_inverse_kinematics, leg_explicit_inverse_kinematics
+from MangDang.mini_pupper.display import Display
+from src.MovementScheme import MovementScheme
+from src.danceSample import MovementLib
 
 config = Configuration()
 state = State()
