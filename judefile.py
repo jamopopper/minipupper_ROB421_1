4976becc0b4375527e2afa6e36f56a8bb2ea8e06
JudeWi <86169929+JudeWi@users.noreply.github.com>
2024-05-14 11:29:50 -0700

    jdue files

---
+++ judefile.py
@@ -0,0 +1,11 @@
+from UDPComms import Publisher
+import time 
+from MangDang.mini_pupper.display import Display
+
+drive_pub = Publisher(8830) 
+
+if __name__ == "__main__":
+    disp = Display()
+    disp.show_image("img\elbow.jpg")
+
+    
