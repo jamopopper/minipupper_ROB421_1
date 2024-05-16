e4a5efe87b2cd59dcddb4023f66c048469489056
sabbekosu <sabbek@oregonstate.edu>
2024-05-16 11:00:06 -0700

    this is it

--- kevinProg.py
+++ kevinProg.py
@@ -1,9 +1,162 @@
 #!/usr/bin/env python3
+from UDPComms import Publisher
+import time 
+
+# drive_pub = Publisher(8830) = controls movement of pupper (basically mode 1)
+# arm_pub = Publisher(8410) = controls more movements of upper (mode 2)
+# mode 2 is what you can do when pupper is not in trot mode when using a controller.
+drive_pub = Publisher(8830) 
+# arm_pub = Publisher(8410)
+# L1 = activate/disactivate
+# R1 = transition between Rest mode and Trot mode.
+# circle = dance or hold for 3 seconds to turn off system
+# trinagle  = NOTHING 
+# X = jump
+# L2 = nothing
+# R2 = Nothing
+# The range for the following are form (-1, 1)
+# ly = forward or backwards
+# lx = left or right
+# rx = turn left or right (pitch)
+# ry = pitches the robot forward
+
+def activate():
+    drive_pub.send({"L1": 1, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+    
+def default():
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+  
+def trot():
+    drive_pub.send({"L1": 0, 
+            "R1": 1, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})  
+    
+def forward():
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 1, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})
+def backward():
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": -1, 
+            "lx": 0, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0}) 
+def right():
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": 1, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0}) 
+def left():
+    drive_pub.send({"L1": 0, 
+            "R1": 0, 
+            "x": 0, 
+            "circle": 0, 
+            "triangle": 0, 
+            "L2": 0, 
+            "R2": 0, 
+            "ly": 0, 
+            "lx": -1, 
+            "rx": 0, 
+            "message_rate": 20, 
+            "ry": 0, 
+            "dpady": 0, 
+            "dpadx": 0})   
+    
 import serial
 if __name__ == '__main__':
-     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.5)
      ser.reset_input_buffer()
      while True:
           if ser.in_waiting > 0:
                line = ser.readline().decode('utf-8').rstrip()
-               print(line)
+               if line == 69:
+                    activate()
+                    print("activated")
+               elif line == 71:
+                    trot()
+                    print("trot")
+               elif line == 70:
+                    forward()
+                    print("forward")
+               elif line == 21:
+                    backward()
+                    print("backward")
+               elif line == 68:
+                    left()
+                    print("left")
+               elif line == 67:
+                    right()
+                    print("right")
+               else:
+                    print("this button does nothing")
+               
+          else:
+              default() 
+              print("defaulted")
