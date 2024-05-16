ddcbe0d12abd4cdf1a5969417523b879bfc8d1a4
jamopopper <jace_palmer@live.com>
2024-05-16 13:55:50 -0700

    dance debug

--- jaceProg.py
+++ jaceProg.py
@@ -70,6 +70,7 @@ def dance(array, frame):
 
     servo_sin = np.sin((3.14) * (frame/256))
     servo_cos = (-np.cos((3.14) * (frame/256)) + 1) / 2
+    print(servo_sin "+" servo_cos)
     store = stand(array, servo_sin, servo_cos)
     return store
 
