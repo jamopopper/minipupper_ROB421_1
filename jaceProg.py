be08c057d80f6750fc0baa54350d5bbd07c600b3
jamopopper <jace_palmer@live.com>
2024-05-16 10:10:56 -0700

    bro let me push!!

--- jaceProg.py
+++ jaceProg.py
@@ -61,20 +61,44 @@ def main(use_imu=False):
     state.joint_angles[0, 1] = 0
     state.joint_angles[0, 2] = 0
     state.joint_angles[0, 3] = 0
+    state.joint_angles[1, 0] = 0
+    state.joint_angles[1, 1] = 0
+    state.joint_angles[1, 2] = 0
+    state.joint_angles[1, 3] = 0
+    state.joint_angles[2, 0] = 0
+    state.joint_angles[2, 1] = 0
+    state.joint_angles[2, 2] = 0
+    state.joint_angles[2, 3] = 0
     hardware_interface.set_actuator_postions(state.joint_angles)
-    time.sleep(1)    
+    time.sleep(2)
+    state.joint_angles[0, 0] = 0
+    state.joint_angles[0, 1] = 0
+    state.joint_angles[0, 2] = 0
+    state.joint_angles[0, 3] = 0
     state.joint_angles[1, 0] = (3.14/4)
     state.joint_angles[1, 1] = (3.14/4)
     state.joint_angles[1, 2] = (3.14/4)
     state.joint_angles[1, 3] = (3.14/4)
-    hardware_interface.set_actuator_postions(state.joint_angles)
-    time.sleep(1)
     state.joint_angles[2, 0] = (3.14/4)
     state.joint_angles[2, 1] = (3.14/4)
     state.joint_angles[2, 2] = (3.14/4)
     state.joint_angles[2, 3] = (3.14/4)
     hardware_interface.set_actuator_postions(state.joint_angles)
-    time.sleep(1)
+    time.sleep(2)
+    state.joint_angles[0, 0] = 0
+    state.joint_angles[0, 1] = 0
+    state.joint_angles[0, 2] = 0
+    state.joint_angles[0, 3] = 0
+    state.joint_angles[1, 0] = (3.14/2)
+    state.joint_angles[1, 1] = (3.14/2)
+    state.joint_angles[1, 2] = (3.14/2)
+    state.joint_angles[1, 3] = (3.14/2)
+    state.joint_angles[2, 0] = (3.14/2)
+    state.joint_angles[2, 1] = (3.14/2)
+    state.joint_angles[2, 2] = (3.14/2)
+    state.joint_angles[2, 3] = (3.14/2)
+    hardware_interface.set_actuator_postions(state.joint_angles)
+    time.sleep(2)
 
     # for iteration in range(1024):
     #     state.joint_angles[1, 0] = (3.14/3)*(iteration/1024) + (3.14/6)
