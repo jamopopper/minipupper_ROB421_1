3f985b7f3525bb72638ffc13afde470a6ee55348
nicbarden <nicbarden1@gmail.com>
2024-05-22 20:36:52 +0000

    -

--- NicProject.py
+++ NicProject.py
@@ -86,7 +86,7 @@ def sidestep0_1(array):
     return store
 
 def sidestep0_2(array, phase, l_r):
-    store = stand(array, 40*phase, 0, phase*20, 0)
+    store = stand(array, 40*phase, 0, -phase*20, 0)
     return store
 
 def sidestep1_1(array):
@@ -94,7 +94,7 @@ def sidestep1_1(array):
     return store
 
 def sidestep1_2(array, phase, l_r):
-    store = stand(array, 40*phase, 0, phase*20, 1)
+    store = stand(array, 40*phase, 0, -phase*20, 1)
     return store
 
 def sidestep2_1(array):
@@ -102,7 +102,7 @@ def sidestep2_1(array):
     return store
 
 def sidestep2_2(array, phase, l_r):
-    store = stand(array, 40*phase, 0, phase*5, 2)
+    store = stand(array, 40*phase, 0, -phase*5, 2)
     return store
 
 def sidestep3_1(array):
@@ -110,7 +110,7 @@ def sidestep3_1(array):
     return store
 
 def sidestep3_2(array, phase, l_r):
-    store = stand(array, 40*phase, 0, phase*5, 3)
+    store = stand(array, 40*phase, 0, -phase*5, 3)
     return store
 
 def sidestep5_1(array):
@@ -118,7 +118,7 @@ def sidestep5_1(array):
     return store
 
 def sidestep5_2(array, phase, l_r):
-    store = stand(array, 40*phase, 0, phase*5, 5)
+    store = stand(array, 40*phase, 0, -phase*5, 5)
     return store
 
 def sidestep6_1(array):
@@ -126,7 +126,7 @@ def sidestep6_1(array):
     return store
 
 def sidestep6_2(array, phase):
-    store = stand(array, 40*phase, 0, phase*5, 6)
+    store = stand(array, 40*phase, 0, -phase*5, 6)
     return store
 
 def main(use_imu=False):
