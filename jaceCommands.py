72d133bc5fe3240a257b6d848d10cd5a355457f0
jamopopper <jace_palmer@live.com>
2024-05-28 10:56:00 -0700

    maybe this works

--- jaceCommands.py
+++ jaceCommands.py
@@ -6,7 +6,7 @@ import numpy as np
 import time
 
 
-def stand(array, height=127, lean=0, roll=0, leg=4): 
+def stand(height=127, lean=0, roll=0, leg=4): 
     # array is the given servo array
     # height (default=127) goes from 0-255
     # lean (default=0) goes from 0-63 positive and negative, positive moves body forward
@@ -19,44 +19,44 @@ def stand(array, height=127, lean=0, roll=0, leg=4):
     # leg 0 is front-right, 1 is front-left, 2 is back-right, 3 is back-left
     # leg 4 is all legs, 5 is front-left and back-right, 6 is front-right and back-left
 
-    copy = array
+    array = np.zeros((3,4))
 
     if leg == 6:
-        copy[0, 1] = (roll/64) * 0.4
-        copy[0, 2] = (roll/64) * 0.4
-        copy[1, 1] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
-        copy[1, 2] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
-        copy[2, 1] = -((3.14/2.7) * ((256-height)/256) + 0.2)
-        copy[2, 2] = -((3.14/2.7) * ((256-height)/256) + 0.2)
+        array[0, 1] = (roll/64) * 0.4
+        array[0, 2] = (roll/64) * 0.4
+        array[1, 1] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
+        array[1, 2] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
+        array[2, 1] = -((3.14/2.7) * ((256-height)/256) + 0.2)
+        array[2, 2] = -((3.14/2.7) * ((256-height)/256) + 0.2)
     elif leg == 5:
-        copy[0, 0] = (roll/64) * 0.4
-        copy[0, 3] = (roll/64) * 0.4
-        copy[1, 0] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
-        copy[1, 3] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
-        copy[2, 0] = -((3.14/2.7) * ((256-height)/256) + 0.2)
-        copy[2, 3] = -((3.14/2.7) * ((256-height)/256) + 0.2)
+        array[0, 0] = (roll/64) * 0.4
+        array[0, 3] = (roll/64) * 0.4
+        array[1, 0] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
+        array[1, 3] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
+        array[2, 0] = -((3.14/2.7) * ((256-height)/256) + 0.2)
+        array[2, 3] = -((3.14/2.7) * ((256-height)/256) + 0.2)
     elif leg == 4:
-        copy[0, 0] = (roll/64) * 0.4
-        copy[0, 1] = (roll/64) * 0.4
-        copy[0, 2] = (roll/64) * 0.4
-        copy[0, 3] = (roll/64) * 0.4
-        copy[1, 0] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
-        copy[1, 1] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
-        copy[1, 2] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
-        copy[1, 3] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
-        copy[2, 0] = -((3.14/2.7) * ((256-height)/256) + 0.2)
-        copy[2, 1] = -((3.14/2.7) * ((256-height)/256) + 0.2)
-        copy[2, 2] = -((3.14/2.7) * ((256-height)/256) + 0.2)
-        copy[2, 3] = -((3.14/2.7) * ((256-height)/256) + 0.2)
+        array[0, 0] = (roll/64) * 0.4
+        array[0, 1] = (roll/64) * 0.4
+        array[0, 2] = (roll/64) * 0.4
+        array[0, 3] = (roll/64) * 0.4
+        array[1, 0] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
+        array[1, 1] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
+        array[1, 2] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
+        array[1, 3] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
+        array[2, 0] = -((3.14/2.7) * ((256-height)/256) + 0.2)
+        array[2, 1] = -((3.14/2.7) * ((256-height)/256) + 0.2)
+        array[2, 2] = -((3.14/2.7) * ((256-height)/256) + 0.2)
+        array[2, 3] = -((3.14/2.7) * ((256-height)/256) + 0.2)
     else:
-        copy[0, leg] = (roll/64) * 0.4
-        copy[1, leg] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
-        copy[2, leg] = -((3.14/2.7) * ((256-height)/256) + 0.2)
+        array[0, leg] = (roll/64) * 0.4
+        array[1, leg] = ((3.14/2.7) * ((256-height)/256) + 0.2 + ((lean/64) * 0.5))
+        array[2, leg] = -((3.14/2.7) * ((256-height)/256) + 0.2)
     
-    return copy
+    return array
 
 
-def walk_control(array, direction, lead_set, frame):
+def walk_control(direction, lead_set, frame):
     # array is the given servo array
     # direction is the direction you want to step toward and goes from 0-1
     # lead_set is the set of feet that are leading the step
@@ -66,36 +66,35 @@ def walk_control(array, direction, lead_set, frame):
     # lead_set when 0 is front-left and back-right, and 1 is front-right and back-left
     # frame when 0 is back step, 0.5 is neutral, and 1 is fully stepped forward
 
-    store = array
+    array = np.zeros((3,4))
 
     if lead_set == 0:
-        store = stand(array, 127 - (np.sin(3.14*frame)), np.sin(6.28 * direction) * 2*(frame-0.5) * 64, np.cos(6.28 * direction) * 2*(frame-0.5) * 64, 5)
-        array = stand(store, 127, -np.sin(6.28 * direction) * 2*(frame-0.5) * 64, -np.cos(6.28 * direction) * 2*(frame-0.5) * 64, 6)
-        store = array
+        array = stand(array, 127 - (np.sin(3.14*frame)), np.sin(6.28 * direction) * 2*(frame-0.5) * 64, np.cos(6.28 * direction) * 2*(frame-0.5) * 64, 5)
+        array = stand(array, 127, -np.sin(6.28 * direction) * 2*(frame-0.5) * 64, -np.cos(6.28 * direction) * 2*(frame-0.5) * 64, 6)
+        array = array
     else:
-        store = stand(array, 127 - (np.sin(3.14*frame)), np.sin(6.28 * direction) * 2*(frame-0.5) * 64, np.cos(6.28 * direction) * 2*(frame-0.5) * 64, 6)
-        array = stand(store, 127, -np.sin(6.28 * direction) * 2*(frame-0.5) * 64, -np.cos(6.28 * direction) * 2*(frame-0.5) * 64, 5)
-        store = array
+        array = stand(array, 127 - (np.sin(3.14*frame)), np.sin(6.28 * direction) * 2*(frame-0.5) * 64, np.cos(6.28 * direction) * 2*(frame-0.5) * 64, 6)
+        array = stand(array, 127, -np.sin(6.28 * direction) * 2*(frame-0.5) * 64, -np.cos(6.28 * direction) * 2*(frame-0.5) * 64, 5)
+        array = array
 
     return array
 
 
-def keyframe(array, duration, end_pos, hw_face):
+def keyframe(duration, start_pos, end_pos, hw_face):
     start_time = time.time()
-    store = np.zeros((3,4))
+    array = np.zeros((3,4))
     while (start_time + duration) > time.time():
     #while (time.time() - start_time) < duration:
         current_step = (time.time() - start_time) / duration
         print(current_step)
         for i in range(3):
             for j in range(4):
-                store[i, j] = (array[i, j] * current_step) + (end_pos[i, j] * (1-current_step))
-        print(store)
-        set_servos(hw_face, store)
+                array[i, j] = (start_pos[i, j] * current_step) + (end_pos[i, j] * (1-current_step))
+        print(array)
+        set_servos(hw_face, array)
 
 
-    store = array
-    return store
+    return array
 
 
 def set_servos(hw_face, state):
@@ -103,14 +102,14 @@ def set_servos(hw_face, state):
     time.sleep(0.05)
     return True
 
-def dance(array, frame): 
-    # array is the given servo array
+def dance(frame): 
     # frame goes from 0-1
 
+    # array is the given servo array
     # moves in a circle
 
     servo_sin = np.sin((6.28) * frame)
     servo_cos = (-np.cos((6.28) * frame) + 1) / 2
-    store = stand(array, height=servo_cos*255, roll=servo_sin*63)
-    return store
+    array = stand(height=servo_cos*255, roll=servo_sin*63)
+    return array
 
