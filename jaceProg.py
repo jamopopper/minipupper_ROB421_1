0718063ebc5ca7376abe6066f347c3ec821e5cb3
jamopopper <jace_palmer@live.com>
2024-05-30 11:51:16 -0700

    bruh

--- jaceProg.py
+++ jaceProg.py
@@ -81,13 +81,13 @@ def main(use_imu=False):
             state.joint_angles = store1
             jc.set_servos(hardware_interface, store1)
             print(i)
-            print(state.joint_angles)
+            print(store1)
         for i in reversed(range(128)):
             store1 = jc.walk_control(0, 0, (i/128))
             state.joint_angles = store1
             jc.set_servos(hardware_interface, store1)
             print(i)
-            print(state.joint_angles)
+            print(store1)
 
         
 
