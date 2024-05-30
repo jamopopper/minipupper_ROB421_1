b3397c329ad6080dab273999069afcc3cc1e5c19
jamopopper <jace_palmer@live.com>
2024-05-30 11:58:54 -0700

    steppin on da beat

--- jaceProg.py
+++ jaceProg.py
@@ -80,10 +80,13 @@ def main(use_imu=False):
             store1 = jc.walk_control(0, 0, (i/128))
             state.joint_angles = store1
             jc.set_servos(hardware_interface, store1)
+            print(store1)
         for i in reversed(range(128)):
             store1 = jc.walk_control(0, 0, (i/128))
             state.joint_angles = store1
             jc.set_servos(hardware_interface, store1)
+            print(store1)
+
 
         
 
