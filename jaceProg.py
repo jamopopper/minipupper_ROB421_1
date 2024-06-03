f5d6c9a3319b26352621c170c5d2e51c5e9b3f25
jamopopper <jace_palmer@live.com>
2024-06-03 12:44:16 -0700

    bro shoould walk

--- jaceProg.py
+++ jaceProg.py
@@ -74,21 +74,17 @@ def main(use_imu=False):
         # store2 = jc.look_right(127)
         # jc.keyframe(2, store1, store2, hardware_interface)
 
-        
-        ## Go thought range of walk stepping
-        for i in range(128):
-            store1 = jc.walk_control(0, 0, (i/128))
-            state.joint_angles = store1
-            jc.set_servos(hardware_interface, store1)
-            print(store1)
-        for i in reversed(range(128)):
-            store1 = jc.walk_control(0, 0, (i/128))
-            state.joint_angles = store1
-            jc.set_servos(hardware_interface, store1)
-            print(store1)
+        jc.walk_control(0, 0.5, 2, hardware_interface)      
+        time.sleep(1)
+        jc.walk_control(1, 0.5, 2, hardware_interface)      
+        time.sleep(1)
+        jc.walk_control(2, 0.5, 2, hardware_interface)      
+        time.sleep(1)
+        jc.walk_control(3, 0.5, 2, hardware_interface)      
+        time.sleep(1)
+        jc.walk_control(0, 1, 6, hardware_interface)      
+        time.sleep(1)
 
-
-        
 
 
 main()
