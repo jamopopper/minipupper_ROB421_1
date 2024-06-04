8c8377afea7b94c545eb3306b10e62ad43eb427d
jamopopper <jace_palmer@live.com>
2024-06-04 11:52:13 -0700

    maybe it works better??

--- jaceCommands.py
+++ jaceCommands.py
@@ -125,10 +125,10 @@ def walk_control(direction, distance, steps, hw_face):
     full_inv_step += stand(127, -np.cos(direction * 6.28) * 63 * distance, -np.sin(direction * 6.28) * 63 * distance, 5, (-np.cos(direction * 6.28) + -np.sin(direction * 6.28)) * 63)
 
     mid_step = stand(127, 0, 0, 5)
-    mid_step += stand(31, 0, 0, 6)
+    mid_step += stand(47, 0, 0, 6)
 
     mid_inv_step = stand(127, 0, 0, 6)
-    mid_inv_step += stand(31, 0, 0, 5)
+    mid_inv_step += stand(47, 0, 0, 5)
 
     
     keyframe(0.2, stationary_step, mid_step, hw_face)
